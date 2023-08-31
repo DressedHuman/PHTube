592240b6c16e5fc694e4bf622b4d6528907377bb
DressedHuman <dressed.human@gmail.com>
2023-09-01 01:55:33 +0600

    finished the loadCategories function with event listener using toggleActiveInactive function created

--- js/index.js
+++ js/index.js
@@ -26,7 +26,35 @@ const loadNav = () => {
 
 // loading the categories buttons
 const loadCategories = () => {
-    console.log("loading categories...");
+    const main = document.getElementById("main");
+    main.innerHTML = `
+        <div id="categories" class="w-2/5 mt-8 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+            <button id="categories-all" class="btn py-2 px-5 active:bg-[#FF1F3D]  active:text-[#FFF] font-medium text-lg active">All</button>
+            <button id="categories-music" class="btn py-2 px-5 active:bg-[#FF1F3D]  active:text-[#FFF] font-medium text-lg inactive">Music</button>
+            <button id="categories-comedy" class="btn py-2 px-5 active:bg-[#FF1F3D]  active:text-[#FFF] font-medium text-lg inactive">Comedy</button>
+            <button id="categories-drawing" class="btn py-2 px-5 active:bg-[#FF1F3D]  active:text-[#FFF] font-medium text-lg inactive">Drawing</button>
+        </div>
+    `;
+    const categoryButtons = main.querySelectorAll("#categories button");
+    toggleActiveInactive(categoryButtons);
+}
+
+
+// helper function to make a button active and the remaining others inactive
+const toggleActiveInactive = elements => {
+    elements.forEach(element => {
+        element.addEventListener("click", () => {
+            // toggling all elements inactive from active
+            elements.forEach(element => {
+                element.classList.remove("active");
+                element.classList.add("inactive");
+            }),
+
+            // toggling the element to active from inactive
+            element.classList.remove("inactive");
+            element.classList.add("active");
+        })
+    });
 }
 
 
