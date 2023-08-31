fd61cc1970a0a37999dd99e6fd0075796d7cfca3
DressedHuman <dressed.human@gmail.com>
2023-08-31 23:12:35 +0600

    created and checked the loadNav function

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ function loadHomepage(){
 
 
 const loadNav = () => {
-    console.log("loading nav bar...");
+    // console.log("loading nav bar...");
+    const header = document.getElementById("header");
+    console.log(header);
 }
 
 
