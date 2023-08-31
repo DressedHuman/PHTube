e23088386f11d9b68e756b6ae8a3b5fe87e96e38
DressedHuman <dressed.human@gmail.com>
2023-09-01 02:46:02 +0600

    created a sample card and loaded the thumbnail

--- js/loadAll.js
+++ js/loadAll.js
@@ -5,6 +5,22 @@ const loadAll = async getUrl => {
     // get the data
     const response = await fetch(getUrl);
     const data = await response.json();
-    data.data.forEach(card => console.log(card));
+    data.data.forEach(cardInfo => {
+        // create a card
+        const card = document.createElement("div");
+        card.classList = `card card-compact w-96 bg-base-100 shadow-xl`;
+        card.innerHTML = `
+            <figure><img src="${cardInfo.thumbnail}" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        `;
+
+        cardsContainer.appendChild(card);
+    });
     
 }
