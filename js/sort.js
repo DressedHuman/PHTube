f541dcfc473c8ee69ea02fd76ff8fca31b217c93
DressedHuman <dressed.human@gmail.com>
2023-09-02 01:06:10 +0600

    skipped the sorting part for now

--- js/sort.js
+++ js/sort.js
@@ -1,1 +1,15 @@
-console.log("sorting cards...");
+const sortCards = async getUrlId => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${getUrlId}`);
+    const responseInJSON = await response.json();
+    const data = responseInJSON.data;
+    data.sort((a, b) => {
+        const aViews = parseFloat(a.others.views.slice(0, -1));
+        const bViews = parseFloat(b.others.views.slice(0, -1));
+        console.log(aViews, bViews);
+        
+        // check and return -1 if aViews>bViews or 1 if aViews<bViews else return 0
+        aViews > bViews ? 1 : (aViews < bViews ? -1 : 0);
+    });
+
+    console.log(data);
+}
