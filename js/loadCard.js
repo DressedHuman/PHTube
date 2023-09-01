a9be2566e9a8ee7b986687188bc68e36fc6b8763
DressedHuman <dressed.human@gmail.com>
2023-09-01 13:25:27 +0600

    added the posted date area

--- js/loadCard.js
+++ js/loadCard.js
@@ -1,12 +1,12 @@
-
 // create loadCard function to load a single card
 const loadCard = (cardInfo, cardsContainer) => {
-        // create a card
-        const card = document.createElement("div");
-        card.classList = "w-[312px] mb-7 mx-auto";
-        card.innerHTML = `
-            <figure>
+    // create a card
+    const card = document.createElement("div");
+    card.classList = "w-[312px] mb-7 mx-auto";
+    card.innerHTML = `
+            <figure class="h-[200px] relative">
                 <img class="rounded-lg h-[200px] w-full" src="${cardInfo.thumbnail}" />
+                <span id="published-date" class="absolute bottom-3 right-3 rounded-sm p-1 text-[#FFF] text-[10px] bg-black"></span>
             </figure>
             <div class="flex items-start gap-3 mt-5">
                 <img src="${cardInfo.authors[0].profile_picture}" style="width: 40px; height: 40px;" class="rounded-full object-cover">
@@ -21,11 +21,30 @@ const loadCard = (cardInfo, cardsContainer) => {
             </div>
         `;
 
-        const verifiedBadge = card.querySelector("#verified");
-        cardInfo.authors[0].verified ? verifiedBadge.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#2568EF" class="w-5 h-5">
+    // show or hide the published date
+    loadPublishedDate(cardInfo, card);
+
+    const verifiedBadge = card.querySelector("#verified");
+    cardInfo.authors[0].verified ? verifiedBadge.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#2568EF" class="w-5 h-5">
         <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
       </svg>      
       ` : verifiedBadge.classList = "hidden";
 
-        cardsContainer.appendChild(card);
-    }
+    cardsContainer.appendChild(card);
+}
+
+
+
+// published date checker function
+const loadPublishedDate = (cardInfo, card) => {
+    const published_date = card.querySelector("#published-date");
+    const posted_date = cardInfo.others.posted_date;
+    // set the time if found or hide the area
+    posted_date ? published_date.innerText = convertTime(posted_date) : published_date.classList.add("hidden");
+}
+
+
+// seconds to dates, hours, minutes converter
+const convertTime = time => {
+    return time;
+}
